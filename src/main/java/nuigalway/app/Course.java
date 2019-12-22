3a0d8a2d8fa25f24e484a1a134f32cdf0541198a
JackSheehan <j.sheehan13@nuigalway.ie>
2019-12-22 22:49:02 +0000

    More tests for Course Class

--- src/main/java/nuigalway/app/Course.java
+++ src/main/java/nuigalway/app/Course.java
@@ -1,12 +1,17 @@
 package nuigalway.app;
 
+import org.joda.time.DateTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
 
-    String courseName;
-    List<Module> moduleList;
+    private String courseName;
+    private List<Module> moduleList;
+    private List<Student> courseStudentList = new ArrayList<>();
+    private DateTime academicStartDate;
+    private DateTime academicEndDate;
 
     public Course() {
 
@@ -31,5 +36,30 @@ public class Course {
 
     public void setModuleList(List<Module> moduleList) {
         this.moduleList = moduleList;
+    }
+
+    public void setAcademicStartDate(DateTime academicStartDate) {
+        this.academicStartDate = academicStartDate;
+    }
+
+    public DateTime getAcademicStartDate() {
+        return academicStartDate;
+    }
+
+    public void setAcademicEndDate(DateTime academicEndDate) {
+        this.academicEndDate = academicEndDate;
+    }
+
+    public DateTime getAcademicEndDate() {
+        return academicEndDate;
+    }
+
+    public List<Student> getStudentList() {
+        for(Module module : getModuleList()) {
+            for(Student student : module.getStudentList()) {
+                courseStudentList.add(student);
+            }
+        }
+        return courseStudentList;
     }
 }
--- src/test/java/nuigalway/app/CourseTest.java
+++ src/test/java/nuigalway/app/CourseTest.java
@@ -15,22 +15,40 @@ public class CourseTest {
     @Before
     public void setUp() {
         DateTime studentDOB = new DateTime(1997, 05, 26, 0, 0);
-        Student testStudent = new Student("Jack", "Sheehan", "16325771", studentDOB);
+        Student testStudent1 = new Student("Jack", "Sheehan", "16325771", studentDOB);
+        Student testStudent2 = new Student("James", "Korshaw", "16325771", studentDOB);
 
         Module module1 = new Module("CT417", "Software Engineering 3");
         Module module2 = new Module("CT421", "Artificial Intelligence");
+
+        module1.addStudent(testStudent1);
+        module2.addStudent(testStudent2);
 
         List<Module> moduleList = new ArrayList<>();
 
         moduleList.add(module1);
         moduleList.add(module2);
 
+        DateTime academicEndDate = new DateTime(2019, 05, 1, 0, 0);
+
         course1.setCourseName("CS&IT");
         course1.setModuleList(moduleList);
+        course1.setAcademicEndDate(academicEndDate);
     }
 
     @Test
     public void getCourseName_hasACourseNameSet_returnsCourseName() {
         assertEquals(course1.getCourseName(), "CS&IT");
+    }
+
+    @Test
+    public void getStudentList_hasModulesAssigned_returnsStudentsEnrolled() {
+        assertEquals(course1.getStudentList().size(), 2);
+    }
+
+    @Test
+    public void getAcademicStartDate_hasAcademicStartDate_returnsAcademicStartDate() {
+        DateTime academicEndDate = new DateTime(2019, 05, 1, 0, 0);
+        assertEquals(course1.getAcademicEndDate(), academicEndDate);
     }
 }
